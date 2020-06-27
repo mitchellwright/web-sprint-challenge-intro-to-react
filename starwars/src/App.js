c35afe38bdfe66c0910a6ccc00cdd9fc6e25d0d1
Mitchell Wright <mitchellbwright@gmail.com>
2020-06-26 18:56:26 -0600

    finish initial version of Pokemon list

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Character from "./components/Character";
+import "./App.css";
+import "nes.css/css/nes.min.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = React.useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+
+  React.useEffect( () => {
+    // make axios call for list of pokemon
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
+      .then( result => {
+        result.data.results.forEach( char => {
+          // make axios call for the individual pokemon's detailed info
+          axios.get(char.url)
+              .then( result => {
+                setCharacters(
+                  // add the pokemon object to the characters array
+                  characters => [...characters, result.data]);
+              })
+              .catch( error => console.error(error));
+        });
+      })
+      .catch( error => {
+        console.error(error);
+      });
+    }, []);
+
+  // if we don't have data yet, show loading message
+  if(!characters) {
+    return (
+      <div className="App">
+        <h1 className="Header">Pokemon Characters</h1>
+        <section className="nes-container">
+          <section className="message-list">
+            <section className="message -left">
+              <i className="nes-bcrikko"></i>
+              <div className="nes-balloon from-left">
+                <p>Getting loaded!</p>
+              </div>
+            </section>
+            <section className="message -right">
+              <div className="nes-balloon from-right">
+                <p>Loading...</p>
+              </div>
+              <i className="nes-charmander"></i>
+            </section>
+          </section>
+        </section>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Pokemon Characters</h1>
+        <div className="character-container">
+          {characters.map( character => {
+              return <Character key={character.id} characterData={character} />
+            }
+          )}
+        </div>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,22 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const CharacterWrapper = styled.div`
+    margin: 25px;
+    width: 30rem;
+`;
+
+const Character = props => {
+    const { name, types, weight, sprites} = props.characterData;
+
+    return (
+        <CharacterWrapper className="nes-container with-title is-centered">
+            <p className="title">{name}</p>
+            <img src={sprites.front_default} alt={`image of ${name}`} />
+            <p>Weight: {weight}</p>
+            <p>Type: {types[0].type.name}</p>
+        </CharacterWrapper>
+    );
+};
+
+export default Character;
